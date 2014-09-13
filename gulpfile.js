4779c90dce51370f8c6fea0d7d55db15d184313a
Sean Clark Hess <seanhess@gmail.com>
2014-09-12 20:42:55 -0600

    gulp + browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,45 @@
 var gulp = require('gulp')
+var browserify = require('gulp-browserify')
+var reactify = require('reactify')
+var gutil = require('gulp-util')
+var rename = require('gulp-rename')
+
+var paths = {
+    // css: ['src/css/**/*.styl'],
+    // index_js: ['./src/js/index.jsx'],
+    index: './public/main.js',
+    js: ['./public/*.js'],
+    jsx: ['./public/*.jsx']
+};
+
+gulp.task('js', function() {
+    browserify(paths.index)
+        // .transform(reactify)
+        .bundle()
+        .pipe(gulp.source('main.js'))
+        .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('browserify', function() {
+  var production = gutil.env.type === 'production'
+
+  gulp.src(['public/index.js'], {read: false})
+
+    // Browserify, and add source maps if this isn't a production build
+    .pipe(browserify({
+      debug: !production,
+      // transform: ['reactify'],
+      extensions: ['.jsx']
+    }))
+
+    // .on('prebundle', function(bundler) {
+    //   // Make React available externally for dev tools
+    //   bundler.require('react');
+    // })
+
+    // Rename the destination file
+    .pipe(rename('bundle.js'))
+
+    // Output to the build directory
+    .pipe(gulp.dest('public/dist/'));
+});
---
+++ public/index.js
@@ -0,0 +1,4 @@
+console.log("HI")
+// var React = require('react');
+// var view = require('./view.jsx'); // need to specify the jsx extension
+// React.renderComponent(view(), document.getElementById('content'));
