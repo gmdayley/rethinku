f5d68c9810ae3397161ac96d27348ec4b0515cb4
Sean Clark Hess <seanhess@gmail.com>
2014-09-12 19:58:50 -0600

    example crud

---
+++ server/model/Course.js
@@ -0,0 +1,33 @@
+var r = require('rethinkdb')
+var db = require('./db')
+
+
+var table = r.table('course')
+
+// set me from the outside
+var run = null
+exports.init = function(r) {
+    run = r
+}
+
+exports.findOne = function(id) {
+    return run(table.get(id))
+}
+
+exports.findAll = function() {
+    return run(table)
+}
+
+exports.insert = function*(item) {
+    delete item.id
+    var result = yield run(table.insert(item))
+    return db.toKey(result)
+}
+
+exports.save = function(id, item) {
+    return run(table.get(id).replace(item))
+}
+
+exports.delete = function(id) {
+    return run(table.get(id).delete())
+}
--- server/server.js
+++ server/server.js
@@ -2,6 +2,22 @@
 'use strict';
 
 var koa = require('koa');
+var Course = require('./model/Course')
+
+var r = require('rethinkdb')
+var db = require('./model/db')
+
+// DATABSE CONNECTION /////////////
+
+var conn = null
+function run(query) {
+    return db.run(conn, query)
+}
+
+r.connect({host:'localhost', port:28015, db:'rethinku'}, function(err, c) {
+    conn = c
+    Course.init(run)
+})
 
 var app = koa();
 
