ee641986e6df9a58b84ecc96527e0959f283b320
Matt Smith <matt.smith123@gmail.com>
2014-09-12 22:22:34 -0600

    base react.js landing page

--- public/js/routes.jsx
+++ public/js/routes.jsx
@@ -1,17 +1,24 @@
 var React = require('react');
 var Router = require('react-router')
 var view = require('./view.jsx')
-var hello = require('./hello.jsx')
+var one = require('./one.jsx')
+var two = require('./two.jsx')
+var courseList = require('./courseList.jsx')
 var notFound = require('./notFound.jsx')
 
 var Route = Router.Route
 var Routes = Router.Routes
+var DefaultRoute = Router.DefaultRoute
+var NotFoundRoute = Router.NotFoundRoute
 var Link = Router.Link
 
 module.exports = (
   <Routes>
-    <Route path="/" handler={view} />
-    <Route path="/test" handler={hello} />
+    <Route name="courses" path="/" handler={view} >
+      <Route name="one" handler={one}/>
+      <Route name="two" handler={two} />
+	  <DefaultRoute handler={courseList}/>
+    </Route >
     <NotFoundRoute handler={notFound} />
   </Routes>
 );
