c1e1c227ce2d38560f6a8273eb2c410a178d31d8
Sean Clark Hess <seanhess@gmail.com>
2014-09-12 22:53:45 -0600

    courses loading

--- public/js/courses/courseList.jsx
+++ public/js/courses/courseList.jsx
@@ -1,34 +1,62 @@
 var React = require('react');
 var $ = require("jquery")
 
-console.log("Check jquery", $)
-
 var MyView = React.createClass({
 
   getInitialState: function() {
-    return {}
+    return {courses:[]}
+  },
+
+  componentDidMount: function() {
+    console.log("MOUNTED")
+    var self = this
+    $.get("/courses", function(result) {
+      console.log("RESULT", result.length)
+      self.setState({courses:result})
+    })
   },
 
   render: function() {
+
+    var list = this.state.courses.map(function(course) {
+      return (
+        <div>
+          <a href="/courses/1234">
+            <span>{course.stem}</span>
+            <span>{course.number}</span>
+          </a>
+        </div>
+      )
+    })
+
+
     return (
-      <table>
-        <tr>
-          <td>one</td>
-          <td>two</td>
-          <td>three</td>
-        </tr>
-        <tr>
-          <td>one</td>
-          <td>two</td>
-          <td>three</td>
-        </tr>
-        <tr>
-          <td>one</td>
-          <td>two</td>
-          <td>three</td>
-        </tr>
-      </table>
-    );
+      <div>
+        <h3>Courses</h3>
+        <div>{list}</div>
+      </div>
+    )
+
+    // return (
+    //   <h3>Courses</h3>
+    //   <table>
+    //     <tr>
+    //       <td>one</td>
+    //       <td>two</td>
+    //       <td>three</td>
+    //     </tr>
+    //     <tr>
+    //       <td>one</td>
+    //       <td>two</td>
+    //       <td>three</td>
+    //     </tr>
+    //     <tr>
+    //       <td>one</td>
+    //       <td>two</td>
+    //       <td>three</td>
+    //     </tr>
+    //   </table>
+    // );
   }
 });
 module.exports = MyView;
